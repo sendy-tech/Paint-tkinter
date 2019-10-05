049ad515583608783b0b5c78b347f79bd2faa0e8
sEndY <1dembel@mail.ru>
2019-10-05 18:39:35 +0300

    Paint v0.01

---
+++ Paint.py
@@ -0,0 +1,49 @@
+from tkinter import *
+
+canvas_width = 700
+canvas_height = 500
+brush_size = 3
+color = "black"
+
+
+def paint(event):
+    global brush_size
+    global color
+    x1 = event.x - brush_size
+    x2 = event.x + brush_size
+    y1 = event.y - brush_size
+    y2 = event.y + brush_size
+    w.create_oval(x1, y1, x2, y2,
+                  fill=color, outline=color)
+
+
+def brish_size_change(new_size):
+    global brush_size
+    brush_size = new_size
+
+
+def color_change(new_color):
+    global color
+    color = new_color
+
+
+root = Tk()
+root.title('Simple Tkinter Paint')
+
+w = Canvas(root,
+           width=canvas_width,
+           height=canvas_height,
+           bg="white")
+w.bind("<B1-Motion>", paint)
+
+w.grid(row=2,
+       column=0,
+       columnspan=7,
+       padx=5,
+       pady=5,
+       sticky=E+W+S+N)
+
+w.columnconfigure(6, weight=1)
+w.rowconfigure(2, weight=1)
+
+root.mainloop()
